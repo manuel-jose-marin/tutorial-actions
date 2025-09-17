5464534e0bf184c4cce8c38193ab82c1bf88a556
manuel-jose-marin <mj.marin@uniandes.edu.co>
2025-09-17 12:53:26 -0500

    Trigger workflow

--- tests/test-persona.py
+++ tests/test-persona.py
@@ -3,7 +3,7 @@ import datetime
 import numpy
 from src.tutorial_actions.persona import Persona
 
-#holaaaaaaHOLAAAAAAAAAAAAAAAAAA12322343242344243
+#COmentarioooooooooooo
 class PersonaTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -55,3 +55,4 @@ class PersonaTestCase(unittest.TestCase):
     def test_instancia_clase(self):
         self.assertIsInstance(self.persona1, Persona)
         self.assertNotIsInstance(self.grupo, Persona)
+# Trigger workflow
