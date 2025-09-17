c49b8449cbd34e15b65190c147fdd864be314b80
manuel-jose-marin <mj.marin@uniandes.edu.co>
2025-09-17 12:00:36 -0500

    mas cambios4

--- tests/test-persona.py
+++ tests/test-persona.py
@@ -54,3 +54,4 @@ class PersonaTestCase(unittest.TestCase):
     def test_instancia_clase(self):
         self.assertIsInstance(self.persona1, Persona)
         self.assertNotIsInstance(self.grupo, Persona)
+
